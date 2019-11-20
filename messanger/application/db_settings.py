5ac137310600346dbfe9577c04952d4c93cab113
IgorKreknin <ikreknin@bk.ru>
2019-11-20 19:25:51 +0300

    Add gitignore

--- messanger/application/db_settings.py
+++
@@ -1,8 +0,0 @@
-default_database_settings = {
-	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'db',
-    'USER': 'igor',
-    'PASSWORD': 'root',
-    'HOST': '127.0.0.1',
-    'PORT': '5432',
-}
